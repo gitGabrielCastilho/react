3461f09a1bb79d89880511adc036553202292e90
Gabriel Castilho <gabrielmcastilho@gmail.com>
2022-05-10 23:43:39 -0300

    inicio da home page, banner e area 1

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,13 +1,37 @@
 import {Container} from './styled'; 
+import { Link } from 'react-router-dom';
 import React from "react";
+import background from "../assets/bg.jpg";
+import f5 from '../assets/f5.jpg';
+import f2 from '../assets/f2.jpg';
+
+
 
 export const Home = () => {
     return (
         <Container>    
-            <section>
+            <section className='banner' style={{ backgroundImage: `url(${background})`}}>
     
-                PAGINA HOME
+                <h1>PAGINA HOME  <br/> DECORAÇOES</h1>
+
+                <span> Deslize para baixo! bla bla bla </span>
         
+            </section>
+            <section className='area1'>
+                
+                <div className='area1--content'>
+                
+                    <div className='bar'></div>
+                    <h2>FALE ALGO SOBRE ISSO</h2>
+                    <div className='area1--quote'><em>"proerd é a solução"</em> - albert einsten</div>
+                    <div className='area1--text'>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur</div>
+                    <button className='bt'><Link to={"/about"}> SAIBA MAIS </Link></button>
+
+                </div>
+
+                <div className='area1--img1' style={{ backgroundImage: `url(${f2})`}}></div>
+                <div className='area1--img2' style={{ backgroundImage: `url(${f5})`}}></div>
+               
             </section>
         </Container>    
     );
--- src/pages/home/styled.js
+++ src/pages/home/styled.js
@@ -1,10 +1,82 @@
 import styled from 'styled-components';
-
 export const Container = styled.div`
 
 min-height: calc(100vh - 250px);
-display: flex;
 width: 100%;
 margin-top: 100px;
+padding-left: 300px;
+padding-right: 300px;
 
+.h2 {
+    font-size: 40px;
+    margin: 0;
+}
+
+.bar {
+    background-color: #709071;
+    width: 168px;
+    height: 22px;
+}
+
+.banner{ 
+    background-position: center;
+    background-size: cover;
+    padding-top: 150px;
+    padding-bottom: 90px;
+    
+}
+
+.banner h1 {
+    font-size: 90px;
+    line-height: 100px;
+    margin: 0;
+    margin-bottom: 100px;
+}
+
+.banner span {
+    font-size: 40px;
+}
+
+.area1{
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+}
+
+.area1--content {
+    padding: 50px;
+}
+
+.area1--img1 {
+    height: 350px;
+    background-size: cover;
+    background-position: center;
+}
+
+.area1--img2 {
+    grid-column: 2 / 3;
+    grid-row: 1 / 3;
+    background-size: cover;
+    background-position: center;
+}
+
+.area1--quote {
+    padding: 15px;
+}
+
+.area1--text {
+    margin-bottom: 30px;
+}
+
+.bt {
+    background-color: white;
+    border: none;   
+}
+
+a {
+    text-decoration:none;
+    color:#000;
+    border: 1px solid;
+    padding: 10px;
+    &: hover {
+        color:#999;}
 `;
