c229acbb2dd75230e7a7139d45194f06737d6987
Gabriel Castilho <gabrielmcastilho@gmail.com>
2022-05-26 19:41:57 -0300

    .

--- src/pages/projetos/index.js
+++ src/pages/projetos/index.js
@@ -1,32 +1,12 @@
 import {Container} from './styled'; 
-import React, {useState} from "react";
-import {Jardim, Quarto, Sala, Cozinha} from './imgProjetos';
-import Todos from './imgProjetos';
+import React from "react";
 
-
-
-export const Projetos = () => {
-    const [active, setActive] = useState("FirstCard")
+export const Projetos= () => {
     return (
         <Container>
-       <div className='todos'>     
-       <nav className='btprojeto'>
-           <button onClick={()=> setActive("Card1")} className='bt'>Todos</button>
-           <button onClick={()=> setActive("Card2")} className='bt'>Sala</button>
-           <button onClick={()=> setActive("Card3")} className='bt'>Cozinha</button>
-           <button onClick={()=> setActive("Card4")} className='bt'>Quarto</button>
-           <button onClick={()=> setActive("Card5")} className='bt'>Jardim</button>
-       </nav>
-       <div className='area--projeto'>
-           {active === "Card1" && <Todos></Todos> }
-           {active === "Card2" && <Sala></Sala> }
-           {active === "Card3" && <Cozinha></Cozinha> }
-           {active === "Card4" && <Quarto></Quarto> }
-           {active === "Card5" && <Jardim></Jardim> }
-           
-       </div>
-      
-       </div>
+        <div className='p-4'>
+            pagina Projetos
+        </div>
         </Container>
     )
 }
--- src/pages/projetos/styled.js
+++ src/pages/projetos/styled.js
@@ -7,30 +7,4 @@ display: flex;
 width: 100%;
 margin-top: 100px;
 
-.todos {
-    margin: 30px auto;
-    
-}
-
-.btprojeto {
-    display: inline-block;
-    margin: 30px 540px;    
-}
-
-.bt {
-    padding: 0 20px;
-    border: 1px solid;
-    cursor: pointer;
-    
-}
-
-
-.bt: first-child {
-    border-radius:30px 0 0 30px;
-}
-
-.bt: last-child {
-    border-radius:0 30px 30px 0;
-}
-
 `;
