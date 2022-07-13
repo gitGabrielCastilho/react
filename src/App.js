730e8cf45d9822e09aed908e5e59eacad5d7179e
Gabriel Castilho <gabrielmcastilho@gmail.com>
2022-07-13 00:01:06 -0300

    começando backend

--- src/App.js
+++ src/App.js
@@ -8,11 +8,21 @@ import Footer from './components/footer';
 
 
 const App = () => {
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+
+
   return (
 
         <Template>
+          <p>{!data ? "Loading..." : data}</p>
           <Header/>
-         
+          
             <Rotas/>
           
           <Footer/>
